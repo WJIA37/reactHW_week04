89dbfabb293ccc1590e59ac8935d5bcd7e1bb0ce
WJIA37 <r810307@yahoo.com.tw>
2025-03-11 16:28:40 +0800

    新增第七週作業

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/reactHW_week04/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/reactHW_week07/' : '/',
   plugins: [react()],
   
 })
